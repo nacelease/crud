bf6c2763742c2e2e2fe7fe5df7435324fa30132b
nacelease <54339008+nacelease@users.noreply.github.com>
2019-10-08 19:15:28 -0500

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CRUD_practice.Data;
+
+namespace CRUD_practice.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CRUD_practice.Data.Departments> Departments { get; set; }
+        public DbSet<CRUD_practice.Data.Employees> Employees { get; set; }
+    }
+}
---
+++ Departments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_practice.Data
+{
+    public class Departments
+    {
+        [Key]
+        [DatabaseGenerated (DatabaseGeneratedOption.Identity)]
+        public int DID { get; set; }
+        [Display(Name ="Department Name")]
+        public string Dname { get; set; }
+        [Display(Name ="Dept Head")]
+        public string DHead { get; set; }
+
+    }
+}
